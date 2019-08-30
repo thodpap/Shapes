85f93db018fb5c9bedf5750ca70290e1271104df
Theodoros Paparrigopoulos <theodoros.paparrigopoulos@ast-engineering.de>
2019-08-30 17:07:42 +0300

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import Shape
+import Shape 
 
 rect = Shape.Rectangle(5,10)
 threeD = Shape.LiveRectangle(3,7,2)
 cube = Shape.Cube(3)
 print(rect.area())
 print(threeD.volume())
-print(cube.volume())
+print(cube.volume()) )
